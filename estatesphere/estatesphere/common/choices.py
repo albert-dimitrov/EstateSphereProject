ed1db3d6cc351f3789ba572c46288589bdc91e10
Albert Dimitrov <dimitrov.albert420@gmail.com>
2024-11-15 16:11:05 +0200

    Fix: Fixed rating choices and base urls import

--- estatesphere/estatesphere/common/choices.py
+++ estatesphere/estatesphere/common/choices.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 
-class RatingChoices(models.TextChoices):
-    ONE_STAR = '1', '1 Star'
-    TWO_STARS = '2', '2 Stars'
-    THREE_STARS = '3', '3 Stars'
-    FOUR_STAR = '4', '4 Stars'
-    FIVE_STARTS = '5', '5 Stars'
+class RatingChoices(models.IntegerChoices):
+    ONE_STAR = 1, '1 Star'
+    TWO_STARS = 2, '2 Stars'
+    THREE_STARS = 3, '3 Stars'
+    FOUR_STAR = 4, '4 Stars'
+    FIVE_STARTS = 5, '5 Stars'
+
+
