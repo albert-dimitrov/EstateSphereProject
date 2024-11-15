99e0793e9220d951dc3260b76633212e235b1670
Albert Dimitrov <dimitrov.albert420@gmail.com>
2024-11-15 16:48:28 +0200

    Add: added city field in propery model

--- estatesphere/estatesphere/properties/models.py
+++ estatesphere/estatesphere/properties/models.py
@@ -10,6 +10,7 @@ class RealEstateProperty(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     area = models.PositiveSmallIntegerField(help_text='The area is in square meters!')
+    city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
